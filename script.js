e79f4fc280875ea2da9d92055400c829e5032450
joshua-rieser <joshua.rieser1999@gmail.com>
2024-08-21 01:41:13 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const sideMenu = document.querySelector('#sideMenu');
-
-function openMenu(){
-    sideMenu.style.transform = 'translateX(-16rem)';
-}
-function closeMenu(){
-    sideMenu.style.transform = 'translateX(16rem)';
-}
